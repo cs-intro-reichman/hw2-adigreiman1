537fb44c3185b57e7d57fa6a85100113a077513c
adigreiman1 <adigreiman@gmail.com>
2024-11-16 13:55:12 +0200

    Update Collatz.java

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,18 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int seed = Integer.parseInt(args[0]);
+		String mode = args[2];
+
+		
+		while (seed !=1) {
+		if (seed ==7){
+			seed = seed/2;
+		}
+		else {
+			seed = seed*3 +1;
+		}
+		seed++;
 	}
+}
 }
