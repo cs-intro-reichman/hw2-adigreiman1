763a2c1d128d92820ff8c757883838a172fbe56f
adigreiman1 <adigreiman@gmail.com>
2024-11-16 15:50:26 +0200

    Update Collatz.java

--- Collatz.java
+++ Collatz.java
@@ -2,17 +2,24 @@
 public class Collatz {
 	public static void main(String args[]) {
 	    int seed = Integer.parseInt(args[0]);
-		String mode = args[2];
+		//String mode = args[2];
+
+	//	if (mode.equals("v")){
 
 		
-		while (seed !=1) {
-		if (seed ==7){
-			seed = seed/2;
+		
+		while (seed>=1){
+			if ( seed == 7){
+				seed/=2;
+				System.out.print(seed);
+				seed++;
+			}
+			else  {
+			seed= seed * 3 + 1;
+			}
+			
+				System.out.print(seed);
+				seed++;
 		}
-		else {
-			seed = seed*3 +1;
-		}
-		seed++;
-	}
-}
-}
+
+		} }
