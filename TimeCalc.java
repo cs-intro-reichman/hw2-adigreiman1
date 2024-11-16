f923483a0f7b79b93819df6c88a9a111f4223a15
adigreiman1 <adigreiman@gmail.com>
2024-11-16 13:54:29 +0200

    Update TimeCalc.java

--- TimeCalc.java
+++ TimeCalc.java
@@ -8,36 +8,28 @@ public class TimeCalc {
         int hours = Integer.parseInt(""+time.charAt(0) +time.charAt(1) );
         int minuts = Integer.parseInt(""+time.charAt(3) +time.charAt(4) );
 
-        if (addition >60 ){
-            int addHour = addition /60;
-            int addMinute  =  addition % 60;
-            hours += addHour;
-            minuts += addMinute;
-            if (minuts>60){
-                minuts = minuts -60;
-                hours +=1;
-            }
-            }
-            else if (addition % 60 ==0){
-                 hours =  addition / 60 + hours;
-            }
-        else{
-            minuts += addition;
-            if (minuts>60){
-                minuts = minuts -60;
-                hours +=1;
-            }
-        }
-            if (hours >= 24){hours = hours-24;}
+        minuts += addition;
+
+        if (minuts >=60){
+            hours = minuts / 60 + hours;
+             minuts = minuts % 60;
+            hours = hours % 24;
+       
         
             if (minuts <10){
-                System.out.println(hours + ":0" + minuts);
+                if (hours<10){
+                    System.out.println("0" +hours + ":0" + minuts);
+                }
+                else{
+                    System.out.println(hours + ":0" + minuts);
+                }
             }
             else {
+                if (hours<10){
+                    System.out.println("0" +hours + ":" + minuts);
+                }
+                else{
                 System.out.println(hours + ":" + minuts);
             }
-        
-    
         }
-        }
-
+        }}}
