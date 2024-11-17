5db48b07a4a2cc07abf62fc3fb7ea4b1d937367e
adigreiman1 <adigreiman@gmail.com>
2024-11-17 14:50:01 +0200

    Update Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -4,15 +4,15 @@ public class Cheers {
                 String word = args[0];
                 String upperWord = word.toUpperCase();
                 int times = Integer.parseInt(args[1]);
-                String vowels = "AEFHILMNORSX";
+                String selectedlLetters  = "AEFHILMNORSX";
                 
                 for (int i =0; i<upperWord.length(); i++){
                         
-                        if (vowels.indexOf(upperWord.charAt(i))>=0){
+                        if (selectedlLetters.indexOf(upperWord.charAt(i))>=0){
                                 System.out.println("Give me an " + upperWord.charAt(i)+": " + upperWord.charAt(i) + "!");
                         }
                         else {
-                        System.out.println("Give me a " + upperWord.charAt(i)+": " + upperWord.charAt(i) + "!"); 
+                        System.out.println("Give me a  " + upperWord.charAt(i)+": " + upperWord.charAt(i) + "!"); 
                         }
                          
                 }
