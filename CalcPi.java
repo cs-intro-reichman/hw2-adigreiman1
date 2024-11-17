bc404fa939e0e271f6d1f68c205f714f46217916
adigreiman1 <adigreiman@gmail.com>
2024-11-17 18:09:01 +0200

    Update CalcPi.java

--- CalcPi.java
+++ CalcPi.java
@@ -5,17 +5,22 @@ public class CalcPi {
 	    int num = Integer.parseInt(args[0]);
 		double PI = Math.PI;
 
-		double output = 0.0; 
-        int i = 1;
-		while (i<num){
-			double sum = 1.0 / (3 + 2 * i) + 1.0 / (5 + 2 * i);
-			output -=  sum;
-
-			i++;
+		double demonstrator = 3; 
+		double finalSum = 1; 
+        
+		for (int i =0; i< num -1; i++){
+			if( i%2 == 0){
+				finalSum = finalSum - (1/demonstrator);
+			}
+			else{
+				finalSum = finalSum + (1/demonstrator);
+			}
+			demonstrator +=2;
 		}
+		
 		System.out.println("pi according to Java:" + PI);
 		
-		System.out.println("pi, approximated:" + output*4);
+		System.out.println("pi, approximated:" + finalSum*4);
 
 
 	}
